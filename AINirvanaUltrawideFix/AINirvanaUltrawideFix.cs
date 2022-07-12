d622ac15dbcd5c3f8be271d7873ffec1d329b5c1
jshethbright <jitarth.sheth00@gmail.com>
2022-07-12 02:21:23 -0400

    Added MSAA fixes and updated cursor visibility fix

--- AINirvanaUltrawideFix/AINirvanaUltrawideFix.cs
+++ AINirvanaUltrawideFix/AINirvanaUltrawideFix.cs
@@ -2,9 +2,9 @@
 using MelonLoader;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering.Universal;
 
-
-[assembly: MelonInfo(typeof(AINirvanaUltrawideFix.AINirvanaUltrawideFix), "AI: Somnium Files 2", "1.0.0", "jshethbright")]
+[assembly: MelonInfo(typeof(AINirvanaUltrawideFix.AINirvanaUltrawideFix), "AI: Somnium Files - nirvanA Initiative", "1.0.0", "jshethbright")]
 [assembly: MelonGame("SpikeChunsoft", "AI_TheSomniumFiles2")]
 namespace AINirvanaUltrawideFix
 {
@@ -16,6 +16,7 @@ namespace AINirvanaUltrawideFix
         public static MelonPreferences_Entry<bool> Fullscreen;
         public static MelonPreferences_Entry<bool> UIFix;
         public static MelonPreferences_Entry<bool> CursorVisible;
+        public static MelonPreferences_Entry<int> MSAA;
 
         public override void OnApplicationStart()
         {
@@ -28,7 +29,9 @@ namespace AINirvanaUltrawideFix
             Fullscreen = Fixes.CreateEntry("Fullscreen", true, "", "Set to true for fullscreen or false for windowed");
             UIFix = Fixes.CreateEntry("UI_Fixes", true, "", "Fixes UI issues at ultrawide/wider");
             CursorVisible = Fixes.CreateEntry("CursorVisible", false, "", "Hides cursor with default (false) value.");
+            MSAA = Fixes.CreateEntry("MSAA", 8, "", "Enable Multisample anti-aliasing (MSAA), [Accepted values are: 0, 2, 4, 8]");
         }
+
 
         [HarmonyPatch]
         public class CustomResolution
@@ -46,10 +49,47 @@ namespace AINirvanaUltrawideFix
                     Screen.SetResolution(DesiredResolutionX.Value, DesiredResolutionY.Value, FullScreenMode.FullScreenWindow);
                 }
 
-                // Set mouse cursor to invisible based on preferences.
-                if (!CursorVisible.Value)
+                
+                QualitySettings.antiAliasing = 8;
+                var pipes = GameObject.FindObjectsOfType<UnityEngine.Rendering.Universal.UniversalRenderPipelineAsset>(true);
+                foreach (var pipe in pipes)
                 {
-                    Cursor.visible = false;
+                    MelonLogger.Msg(pipe.m_MSAA);
+                    pipe.m_MSAA = UnityEngine.Rendering.Universal.MsaaQuality._8x;
+                    //GraphicsSettings.renderPipelineAsset = pipe;
+
+                }
+            }
+
+            
+        }
+
+        // Fix in-game MSAA
+        [HarmonyPatch]
+        public class MSAAFix
+        {
+            [HarmonyPatch(typeof(UniversalRenderPipelineAsset), "CreatePipeline")]
+            [HarmonyPrefix]
+            public static void fixPipeline(UniversalRenderPipelineAsset __instance)
+            {
+                //MelonLogger.Msg($"{__instance.name}: {__instance.GetInstanceID()}");
+                switch (MSAA.Value)
+                {
+                    case 0:
+                        __instance.m_MSAA = MsaaQuality.Disabled;
+                        break;
+                    case 2:
+                        __instance.m_MSAA = MsaaQuality._2x;
+                        break;
+                    case 4:
+                        __instance.m_MSAA = MsaaQuality._4x;
+                        break;
+                    case 8:
+                        __instance.m_MSAA = MsaaQuality._8x;
+                        break;
+                    default:
+                        __instance.m_MSAA = MsaaQuality.Disabled;
+                        break;
                 }
             }
         }
@@ -58,6 +98,23 @@ namespace AINirvanaUltrawideFix
         public class UIFixes
         {
             public static float NewAspectRatio = (float)DesiredResolutionX.Value / DesiredResolutionY.Value;
+
+            // Leave cursor visible at main menu
+            [HarmonyPatch(typeof(CanvasScaler), "OnEnable")]
+            [HarmonyPostfix]
+            public static void test()
+            {
+                GameObject uiActive = GameObject.Find("Canvas2");
+
+                if (uiActive != null && Cursor.visible == true)
+                {
+                    if (!CursorVisible.Value)
+                    {
+                        Cursor.visible = false;
+                    }
+                }
+            }
+
 
             // Implement various UI scaling fixed by changing ScreenMatchMode and scaling filters for specific canvases.
             [HarmonyPatch(typeof(CanvasScaler), "OnEnable")]
@@ -77,10 +134,10 @@ namespace AINirvanaUltrawideFix
 
                     else if (currName == "#Canvas")
                     {
+                        
+
                         Canvas currCanvas = __instance.gameObject.GetComponent<Canvas>();
                         Transform[] baseChildren = currCanvas.GetComponentsInChildren<Transform>(false);
-
-                        
                         
                         foreach (Transform item in baseChildren)
                         {
@@ -88,7 +145,7 @@ namespace AINirvanaUltrawideFix
                             string itemName = currObj.name;
                             if (itemName.Contains("Filter"))
                             {
-                                MelonLogger.Msg(itemName);
+                                
                                 currObj.transform.localScale = new Vector3(1 * NewAspectRatio, 1f, 1f);
                             }
                         }
@@ -100,6 +157,7 @@ namespace AINirvanaUltrawideFix
                         __instance.matchWidthOrHeight = 1;
                         Canvas currCanvas = __instance.gameObject.GetComponent<Canvas>();
 
+
                         Transform[] allChildren = currCanvas.GetComponentsInChildren<Transform>(false);
 
                         foreach (Transform item in allChildren)
@@ -108,7 +166,7 @@ namespace AINirvanaUltrawideFix
                             string itemName = currObj.name;
                             if (itemName.Contains("Filter"))
                             {
-                                MelonLogger.Msg(itemName);
+                                //MelonLogger.Msg(itemName);
                                 currObj.transform.localScale = new Vector3(1 * NewAspectRatio, 1f, 1f);
                             }
                         }
@@ -118,6 +176,7 @@ namespace AINirvanaUltrawideFix
                     {
                         __instance.m_ScreenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
                     }
+                    
                 }
             }
 
